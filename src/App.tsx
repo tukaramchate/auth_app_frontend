8bc7fa7109fe9ecc4345735a04204528827acafb
tukaramchate <tukaramchate397@gmail.com>
2025-12-21 14:38:21 +0530

    project setup done

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './index.css'
+import { Button } from './components/ui/button'
+import { Calendar } from './components/ui/calendar'
+import { Label } from './components/ui/label'
+
+function App() {
+
+  return (
+    <div>
+      <Button> Click me </Button>
+      <Calendar />
+      <Label> Sample Label </Label>
+      </div>
+  )
+}
+
+export default App
